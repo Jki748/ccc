a04852b1b607c6d712a004721b97383231a1f21b
Jki748 <94294136+Jki748@users.noreply.github.com>
2023-12-21 22:56:42 +0300

    Update laba4.c

--- laba4.c
+++ laba4.c
@@ -3,23 +3,22 @@
 #include <locale.h>
 
 void main(void) {
+    setlocale(LC_ALL, "RUS");
+    int c, i, ch;
+    i = 0;
+    c = 0;
 
-
-setlocale(LC_ALL, "RUS");
-int c, i, ch;
-i = 0;
-c = 0;
-
-for (i = 0; i < 7; i++) {
-
-ch = getchar();
-if (ch == 'x')
-c++;
-printf ("%c\n", ch);
+    for (i = 0; i < 30; i++) {
+        ch = getchar();
+    if (ch == 'x')
+        c++;
+    if (ch = '\n')
+        break;
+    //printf ("%c\n", ch);
 
 }
-if (c == 0)
-printf("нет х");
-else
-printf("%d\n", c);
+    if (c == 0)
+        printf("нет х");
+    else
+        printf("%d\n", c);
 }
